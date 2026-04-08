c897a6cb2185c971ff17a9fe6358850a63d39f0e
harish111992 <63441336+harish111992@users.noreply.github.com>
2026-04-08 16:04:12 +0530

    Update awk.sh

--- awk.sh
+++ awk.sh
@@ -1,3 +1,5 @@
+
+#Script to fetch the Highest swapMemoery of process
 for pid in $(ls /proc | grep -E '^[0-9]+$'); do
   awk -v pid=$pid '/VmSwap/ {if ($2 > 0) print $2 " KB\t" pid}' /proc/$pid/status 2>/dev/null
 done | sort -n | tail -10
