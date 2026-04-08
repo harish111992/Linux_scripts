970978c88f3111fa6f758031e4b997751ecb3575
harish111992 <63441336+harish111992@users.noreply.github.com>
2026-04-08 16:03:26 +0530

    Update awk.sh

--- awk.sh
+++ awk.sh
@@ -8,3 +8,9 @@ done | sort -n | tail -10
 #1
 #2
 #10
+########
+#awk -v pid=$pid '/VmSwap/ {if ($2 > 0) print $2 " KB\t" pid}' /proc/$pid/status 2>/dev/null
+#-v = pass shell variables to the awk, beacuse awk runs out of the shell
+#so -v pid=$pid which means we are informing the same to awk
+# /vmSwap/ => Looks for the line and prints secons coloumn
+# pid of the shell passes to the awk
