2c291bc50734eb6568ce82e14d3b2bb67f9db498
harish111992 <63441336+harish111992@users.noreply.github.com>
2026-04-08 16:00:18 +0530

    Update awk.sh

--- awk.sh
+++ awk.sh
@@ -2,3 +2,9 @@ for pid in $(ls /proc | grep -E '^[0-9]+$'); do
   awk -v pid=$pid '/VmSwap/ {if ($2 > 0) print $2 " KB\t" pid}' /proc/$pid/status 2>/dev/null
 done | sort -n | tail -10
 
+#######
+# Last line has the highest number
+# sort -n
+#1
+#2
+#10
